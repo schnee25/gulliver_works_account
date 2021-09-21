65f7921903e7dd238788f13e2866102f11720587
schnee25 <schnee.2114@gmail.com>
2021-09-21 17:03:43 +0900

    メールアドレスに誤りがある場合のエラーメッセージを表示できるように

--- src/scenes/signIn/index.tsx
+++ src/scenes/signIn/index.tsx
@@ -6,7 +6,7 @@ import styles from "./style.module.scss";
 import { SignInParams, useSignInPresenter } from "./useSignInPresenter";
 
 const SignInPage = () => {
-  const { register, handleSubmit } = useForm<SignInParams>();
+  const { register, handleSubmit, errors } = useForm<SignInParams>();
   const { signIn } = useSignInPresenter();
   const { account } = useCurrentAccount();
   const history = useHistory();
@@ -16,37 +16,51 @@ const SignInPage = () => {
   }, [account]);
 
   const onSubmit = (data: SignInParams) => {
+    console.log("hoge");
+
     signIn(data);
   };
+
+  console.log(errors);
+
   return (
     <div className={styles.page}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <h1>求職者ログイン</h1>
-        <tr>
+        <div>
+          {/* trをdivに */}
           <th>メールアドレス</th>
+          {/* 正しいメールアドレスでない場合正しいメールアドレスでありませんというエラーメッセージ */}
           <input
             type="text"
             className={styles.InputMail}
             name="account.email"
-            ref={register}
+            ref={register({
+              required: "メールアドレスを正しく入力して下さい",
+              pattern: {
+                value: /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                message: "メールアドレスを正しく入力して下さい",
+              },
+            })}
             placeholder="coadmap@mail.com"
           />
-        </tr>
-        <tr>
+          {errors.account?.email && <p className="formError">{errors.account?.email.message}</p>}
+        </div>
+        <div className={styles.logInItem}>
           <th>パスワード</th>
           <input
             type="password"
             className={styles.InputPass}
             name="account.password"
             ref={register}
-            placeholder="パスワードを入力" // type="textにする？"
+            placeholder="パスワードを入力" // type="textにする？" ここが✴︎になっちゃう
+            minLength={6}
           />
-        </tr>
-
+        </div>
         <button className={styles.loginBtn}>ログイン</button>
-        <button className={styles.forgetPassBtn}>パスワードを忘れた方はこちら</button>
-        <button className={styles.registerBtn}>新規登録はこちら</button>
       </form>
+      <button className={styles.forgetPassBtn}>パスワードを忘れた方はこちら</button>
+      <button className={styles.registerBtn}>新規登録はこちら</button>
     </div>
   );
 };
