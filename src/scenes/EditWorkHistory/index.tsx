ef18445d14e627f147e4f19e55a0c53d6b5af478
schnee25 <schnee.2114@gmail.com>
2021-10-24 17:37:52 +0900

    add template

--- src/scenes/EditWorkHistory/index.tsx
+++ src/scenes/EditWorkHistory/index.tsx
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from "react";
 import styles from "./style.module.scss";
-import axios from "axios";
+import { WorkHistoryType } from "../../data/WorkHistory";
+import { HttpClient } from "../../utilities/axiosInstance";
+import { useCurrentAccount } from "../../hooks/useCurrentAccount";
+import { APIHost } from "../../utilities/constants";
 
 const EditWorkHistory = () => {
+  const [workHistories, setWorkHistory] = useState<WorkHistoryType[]>();
+
+  const [open, setOpen] = React.useState(false);
+  const [post, setPosts] = useState([]);
+  const { account } = useCurrentAccount();
+  useEffect(() => {
+    HttpClient.request({
+      method: "GET",
+      url: `${APIHost.APP}/accounts/${account?.id}`,
+      // url: "https://fed79e73-d600-4c5a-8f45-dfa52cb9d13a.mock.pstmn.io/accounts",
+    }).then((res) => {
+      setWorkHistory(res.data.workHistories);
+    });
+  }, []);
+
   return (
-    <div>
+    <div className={styles.page}>
       <div></div>
     </div>
   );
--- src/scenes/editAcademicBackground/index.tsx
+++ src/scenes/editAcademicBackground/index.tsx
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from "react";
 import styles from "./style.module.scss";
-import axios from "axios";
+import { AcademicBackgroundType } from "../../data/AcademicBackground";
+import { HttpClient } from "../../utilities/axiosInstance";
+import { useCurrentAccount } from "../../hooks/useCurrentAccount";
+import { APIHost } from "../../utilities/constants";
 
 const editAcademicBackground = () => {
+  const [academicHistories, setAcademicBackground] = useState<AcademicBackgroundType[]>();
+  const [open, setOpen] = React.useState(false);
+  const [post, setPosts] = useState([]);
+  const { account } = useCurrentAccount();
+  useEffect(() => {
+    HttpClient.request({
+      method: "GET",
+      url: `${APIHost.APP}/accounts/${account?.id}`,
+      // url: "https://fed79e73-d600-4c5a-8f45-dfa52cb9d13a.mock.pstmn.io/accounts",
+    }).then((res) => {
+      setAcademicBackground(res.data.academicHistories);
+    });
+  }, []);
+
   return (
-    <div>
+    <div className={styles.page}>
       <div></div>
     </div>
   );
