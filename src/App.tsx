489faa88a47353d59cb7892b41bccb755086841e
schnee25 <schnee.2114@gmail.com>
2021-10-24 17:05:05 +0900

    move files to edit the modal of jobseeker'sProfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React, { FC } from "react";
 import "./App.scss";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import RecruitmentIndexPage from "./scenes/recruitment/RecruitmentIndexPage";
 import SignInPage from "./scenes/signIn";
 import ApplicantMyPage from "./scenes/myPage";
+import EditBasicInfo from "./scenes/editBasicInfo";
 
 const App: FC = () => (
   <BrowserRouter>
     <div>
-      <Route exact path="/" component={RecruitmentIndexPage} />
-      <Route path="/sign_in" component={SignInPage} />
-      <Route path="/my_page" component={ApplicantMyPage} />
+      <Switch>
+        <Route exact path="/" component={RecruitmentIndexPage} />
+        <Route path="/sign_in" component={SignInPage} />
+        <Route path="/my_page" component={ApplicantMyPage} />
+        <Route path="/edit_basic_info" component={EditBasicInfo} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
--- src/scenes/mypage/editBasicInfo/index.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useState } from "react";
-import styles from "./style.module.scss";
-import axios from "axios";
-
-const EditBasicInfo = () => {};
-export default EditBasicInfo;
