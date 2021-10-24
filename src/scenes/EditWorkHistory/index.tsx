c91f161206873d26545a9ac5cc565385edac0220
schnee25 <schnee.2114@gmail.com>
2021-10-24 17:57:02 +0900

    edit the files of work history

--- src/scenes/EditWorkHistory/index.tsx
+++ src/scenes/EditWorkHistory/index.tsx
@@ -7,7 +7,6 @@ import { APIHost } from "../../utilities/constants";
 
 const EditWorkHistory = () => {
   const [workHistories, setWorkHistory] = useState<WorkHistoryType[]>();
-
   const [open, setOpen] = React.useState(false);
   const [post, setPosts] = useState([]);
   const { account } = useCurrentAccount();
@@ -20,10 +19,27 @@ const EditWorkHistory = () => {
       setWorkHistory(res.data.workHistories);
     });
   }, []);
-
+  console.log(workHistories);
   return (
     <div className={styles.page}>
-      <div></div>
+      <div className={styles.header}>
+        <h1>職歴</h1>
+      </div>
+      <div className={styles.main}>
+        <h2 className={styles.contentsTitle}>企業名</h2>
+        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+        <h2 className={styles.contentsTitle}>部署･役職</h2>
+        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+        <h2 className={styles.contentsTitle}>日程</h2>
+        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+        <h2 className={styles.contentsTitle}>職歴</h2>
+        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+      </div>
+      <div className={styles.buttons}>
+        <button id="delete">削除する</button>
+        <button id="delete">キャンセル</button>
+        <button id="update">更新</button>
+      </div>
     </div>
   );
 };
--- src/scenes/addWorkHistory/index.tsx
+++ src/scenes/addWorkHistory/index.tsx
@@ -6,23 +6,25 @@ import { useCurrentAccount } from "../../hooks/useCurrentAccount";
 import { APIHost } from "../../utilities/constants";
 
 const editWorkHistory = () => {
-  const [workHistories, setWorkHistory] = useState<WorkHistoryType[]>();
-  const [open, setOpen] = React.useState(false);
-  const [post, setPosts] = useState([]);
-  const { account } = useCurrentAccount();
-  useEffect(() => {
-    HttpClient.request({
-      method: "GET",
-      url: `${APIHost.APP}/accounts/${account?.id}`,
-      // url: "https://fed79e73-d600-4c5a-8f45-dfa52cb9d13a.mock.pstmn.io/accounts",
-    }).then((res) => {
-      setWorkHistory(res.data.workHistories);
-    });
-  }, []);
-
   return (
     <div className={styles.page}>
-      <div></div>
+      <div className={styles.header}>
+        <h1>職歴</h1>
+      </div>
+      <div className={styles.main}>
+        <h2 className={styles.contentsTitle}>企業名</h2>
+        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+        <h2 className={styles.contentsTitle}>部署･役職</h2>
+        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+        <h2 className={styles.contentsTitle}>日程</h2>
+        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+        <h2 className={styles.contentsTitle}>職歴</h2>
+        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+      </div>
+      <div className={styles.buttons}>
+        <button id="delete">キャンセル</button>
+        <button id="update">更新</button>
+      </div>
     </div>
   );
 };
