a5200001ac27f8dd7b79d551650573b2953456f9
schnee25 <schnee.2114@gmail.com>
2021-10-25 13:27:17 +0900

    update files of addWorkHistory

--- src/scenes/addWorkHistory/index.tsx
+++ src/scenes/addWorkHistory/index.tsx
@@ -4,6 +4,7 @@ import { WorkHistoryType } from "../../data/WorkHistory";
 import { HttpClient } from "../../utilities/axiosInstance";
 import { useCurrentAccount } from "../../hooks/useCurrentAccount";
 import { APIHost } from "../../utilities/constants";
+import Button from "../../components/modalEditButton/CustomButtonComponent";
 
 const editWorkHistory = () => {
   return (
@@ -13,17 +14,21 @@ const editWorkHistory = () => {
       </div>
       <div className={styles.main}>
         <h2 className={styles.contentsTitle}>企業名</h2>
-        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+        <input className={styles.contents} type="text" name="" id="" />
         <h2 className={styles.contentsTitle}>部署･役職</h2>
-        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+        <input className={styles.contents} type="text" name="" id="" />
         <h2 className={styles.contentsTitle}>日程</h2>
-        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+        <input className={styles.contents} type="date" name="" id="" />
         <h2 className={styles.contentsTitle}>職歴</h2>
-        {/* <p className={styles.contents}>{workHistories.name}</p> */}
+        <input className={styles.contents} type="text" name="" id="" />
       </div>
       <div className={styles.buttons}>
-        <button id="delete">キャンセル</button>
-        <button id="update">更新</button>
+        <Button color="#05C757" onClick={() => console.log("cancel")} id={"delete"}>
+          キャンセル
+        </Button>
+        <Button color="#05C757" onClick={() => console.log("update")} id={"update"}>
+          更新
+        </Button>
       </div>
     </div>
   );
