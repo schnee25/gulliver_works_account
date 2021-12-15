c7473545255477e3b2d03981818d7231be47a328
schnee25 <schnee.2114@gmail.com>
2021-12-15 17:08:03 +0900

    変更を取得する関数を追加

--- src/scenes/mypage/modals/editBasicInfoModal.tsx
+++ src/scenes/mypage/modals/editBasicInfoModal.tsx
@@ -20,6 +20,32 @@ interface Props {
 }
 
 const EditBasicInfoModal: React.FC<Props> = ({ profiles, visible, onClick }) => {
+  const [name, setName] = useState("");
+  const handleLastNameChange = (event: any) => {
+    setName(event.target.value);
+    const changedLastName = event.target.value;
+    console.log(changedLastName);
+  };
+  const handleFirstNameChange = (event: any) => {
+    setName(event.target.value);
+    const changedFirstName = event.target.value;
+    console.log(changedFirstName);
+  };
+  const handleAddressChange = (event: any) => {
+    setName(event.target.value);
+    const changedAddress = event.target.value;
+    console.log(changedAddress);
+  };
+  const handleGenderChange = (event: any) => {
+    setName(event.target.value);
+    const changedGender = event.target.value;
+    console.log(changedGender);
+  };
+
+  const handleSubmit = () => {
+    console.log(name);
+  };
+
   return (
     <React.Fragment>
       {/* プロフィール */}
@@ -31,57 +57,76 @@ const EditBasicInfoModal: React.FC<Props> = ({ profiles, visible, onClick }) => 
         destroyOnClose={true}
         closable={false}
       >
-        <div className={styles.modalHeader}>
-          <h1>プロフィール</h1>
-          <div className={styles.modalProfileTopImages}>
-            <img src={profileBackground} alt="" className={styles.modalProfileBackground} />
-            <img src={profileIcon} alt="" className={styles.modalProfileIcon} />
+        <form
+          action="https://fed79e73-d600-4c5a-8f45-dfa52cb9d13a.mock.pstmn.io/accounts"
+          method="post"
+          className={styles.modalForm}
+        >
+          <div className={styles.modalHeader}>
+            <h1>プロフィール</h1>
+            <div className={styles.modalProfileTopImages}>
+              <img src={profileBackground} alt="" className={styles.modalProfileBackground} />
+              <img src={profileIcon} alt="" className={styles.modalProfileIcon} />
+            </div>
           </div>
-        </div>
-        <div className={styles.modalMain}>
-          <h2 className={styles.modalContentsTitle}>名前</h2>
-          <div className={styles.modalProfileName}>
+          <div className={styles.modalMain}>
+            <h2 className={styles.modalContentsTitle}>名前</h2>
+            <div className={styles.modalProfileName}>
+              <input
+                className={styles.modalContents}
+                id="lastName"
+                type="text"
+                defaultValue={profiles?.lastName}
+                onChange={handleLastNameChange}
+              />
+              <input
+                className={styles.modalContents}
+                id="firstName"
+                type="text"
+                defaultValue={profiles?.firstName}
+                onChange={handleFirstNameChange}
+              />
+            </div>
+            <h2 className={styles.modalContentsTitle}>住まい</h2>
             <input
               className={styles.modalContents}
-              id="lastName"
               type="text"
-              value={profiles?.lastName}
+              defaultValue={profiles?.address}
+              onChange={handleAddressChange}
             />
+            <h2 className={styles.modalContentsTitle}>性別</h2>
             <input
               className={styles.modalContents}
-              id="firstName"
               type="text"
-              value={profiles?.firstName}
+              defaultValue={profiles?.gender}
+              onChange={handleGenderChange}
             />
+            <h2 className={styles.modalContentsTitle}>日程</h2>
+            <input className={styles.modalContents} type="date" name="" id="" />
           </div>
-          <h2 className={styles.modalContentsTitle}>住まい</h2>
-          <input className={styles.modalContents} type="text" value={profiles?.address} />
-          <h2 className={styles.modalContentsTitle}>性別</h2>
-          <input className={styles.modalContents} type="text" value={profiles?.gender} />
-          <h2 className={styles.modalContentsTitle}>日程</h2>
-          <input className={styles.modalContents} type="date" name="" id="" />
-        </div>
 
-        <div className={styles.modalButtons}>
-          <div className={styles.modalButtonsLeft}></div>
-          <div className={styles.modalButtonsRight}>
-            <CustomButton
-              //   onClick={toggleIsEditBasicInfoModalModal}
-              onClick={onClick}
-              className={styles.modalButton}
-              color="gray"
-            >
-              キャンセル
-            </CustomButton>
-            <CustomButton
-              color="green"
-              className={styles.modalButton}
-              onClick={() => console.log("update")}
-            >
-              更新
-            </CustomButton>
+          <div className={styles.modalButtons}>
+            <div className={styles.modalButtonsLeft}></div>
+            <div className={styles.modalButtonsRight}>
+              <CustomButton
+                //   onClick={toggleIsEditBasicInfoModalModal}
+                onClick={onClick}
+                className={styles.modalButton}
+                color="gray"
+              >
+                キャンセル
+              </CustomButton>
+              <CustomButton
+                color="green"
+                className={styles.modalButton}
+                // onClick={() => console.log("update")}
+                onClick={handleSubmit}
+              >
+                更新
+              </CustomButton>
+            </div>
           </div>
-        </div>
+        </form>
       </Modal>
     </React.Fragment>
   );
